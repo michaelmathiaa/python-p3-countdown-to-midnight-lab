f6e3c1cad497f5342e0b9cfe6698c3e342ed9248
Michael Mathias <michaelscodingjourney@gmail.com>
2023-07-24 21:23:42 -0700

    Used while loops and sleep to pass tests

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
-# your code goes here!
+import time
+
+def countdown(number):
+    while number >= 1:
+        print(f"{number} SECOND(S)!")
+        number -= 1
+    print("HAPPY NEW YEAR!")
+        
+
+def countdown_with_sleep(number):
+    while number >= 1:
+        print(f"{number} SECOND(S)!")
+        number -= 1
+    print("HAPPY NEW YEAR!")
+    time.sleep(1)
